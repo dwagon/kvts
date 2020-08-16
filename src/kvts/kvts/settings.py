ba719d4492a9506e30a203d55394af474948847a
Dougal Scott <dougal.scott@gmail.com>
2020-08-16 20:03:57 +1000

    Use bootstrap4

--- src/kvts/kvts/settings.py
+++ src/kvts/kvts/settings.py
@@ -34,6 +34,7 @@ INTERNAL_IPS = [
 # Application definition
 INSTALLED_APPS = [
     'timesheet',
+    'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
